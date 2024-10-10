a87a9059b40dce19fd195ff23826be76c8d89888
appl4e <appl4e@gmail.com>
2024-10-10 16:31:51 +0600

    Phone preview component wip

---
+++ src/components/PhonePreview.tsx
@@ -0,0 +1,11 @@
+export const PhonePreview = () => {
+	return (
+		<div className="flex items-center justify-center py-[102px]">
+			<div className="phone-bg px-[36px] py-[70px]">
+				<div className="rounded-full bg-muted border-4 border-primary size-[105px] overflow-hidden flex items-center justify-center mx-auto">
+					<img src="/images/profile.png" alt="Profile Pic" className="size-full" />
+				</div>
+			</div>
+		</div>
+	);
+};
--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,12 +1,18 @@
 import { Header } from "@/components/Header";
+import { PhonePreview } from "@/components/PhonePreview";
 import { Outlet } from "react-router-dom";
 
 export const AppLayout = () => {
 	return (
 		<div className="p-6 bg-background">
 			<Header />
-			<main>
-				<Outlet />
+			<main className="py-6 md:grid grid-cols-12 gap-x-24">
+				<div className="col-span-5 bg-white rounded-lg">
+					<PhonePreview />
+				</div>
+				<div className="col-span-7">
+					<Outlet />
+				</div>
 			</main>
 		</div>
 	);
