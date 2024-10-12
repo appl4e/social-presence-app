a2d090642d9f2bba4e61fa0c0c0da651b092a094
appl4e <appl4e@gmail.com>
2024-10-12 17:34:26 +0600

    links page form validation and submit

---
+++ src/pages/LinkFields.tsx
@@ -0,0 +1,87 @@
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import { Button, Group, Input, Select, SelectProps } from "@mantine/core";
+import { useFormContext } from "react-hook-form";
+import { FaLink } from "react-icons/fa";
+import { HiMiniBars2 } from "react-icons/hi2";
+import { ImLinkedin } from "react-icons/im";
+import { IoLogoYoutube } from "react-icons/io";
+import { TbBrandGithubFilled } from "react-icons/tb";
+import { LinksFormType } from "./links.page";
+
+export const LinkFields = ({ id, index, remove, item }) => {
+	const {
+		register,
+		formState: { errors },
+		setValue,
+		clearErrors,
+	} = useFormContext<LinksFormType>();
+
+	const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: id });
+
+	const style = {
+		transform: CSS.Transform.toString(transform),
+		transition,
+	};
+
+	const platformData = [
+		{ value: "github", label: "Github" },
+		{ value: "youtube", label: "Youtube" },
+		{ value: "linkedin", label: "Linkedin" },
+	];
+
+	const icons: Record<string, React.ReactNode> = {
+		github: <TbBrandGithubFilled />,
+		youtube: <IoLogoYoutube />,
+		linkedin: <ImLinkedin />,
+	};
+
+	const renderSelectOption: SelectProps["renderOption"] = ({ option }) => (
+		<Group flex="1" gap="xs">
+			{icons[option.value]}
+			{option.label}
+		</Group>
+	);
+
+	return (
+		<div className="bg-background rounded-lg p-5 mt-6 space-y-3" id={id} ref={setNodeRef} style={style} {...attributes} {...listeners}>
+			<div className="flex items-center">
+				<Button variant="transparent" color="muted" className="flex items-center cursor-pointer" leftSection={<HiMiniBars2 />}>
+					Link #{index + 1}
+				</Button>
+				{index < 1 ? (
+					""
+				) : (
+					<Button variant="transparent" color="muted" className="ms-auto" classNames={{ label: "font-normal" }} onClick={() => remove(index)}>
+						Remove
+					</Button>
+				)}
+			</div>
+			<Select
+				label="Platform"
+				size="lg"
+				value={item.platform}
+				placeholder="Select your platform"
+				leftSection={icons[item.platform]}
+				data={platformData}
+				renderOption={renderSelectOption}
+				{...register(`links.${index}.platform`, { required: true })}
+				onChange={(value) => {
+					setValue(`links.${index}.platform`, value || "");
+					if (value) {
+						clearErrors(`links.${index}.platform`);
+					}
+				}}
+				error={errors?.links?.length ? errors?.links[index]?.platform?.message : ""}
+				classNames={{
+					root: "relative",
+					error: "text-sm",
+				}}
+			/>
+
+			<Input.Wrapper label="Link" error={errors?.links?.length ? errors?.links[index]?.link?.message : ""}>
+				<Input type="text" size="lg" {...register(`links.${index}.link`, { required: true })} leftSection={<FaLink />} />
+			</Input.Wrapper>
+		</div>
+	);
+};
--- src/pages/links.page.tsx
+++ src/pages/links.page.tsx
@@ -1,12 +1,12 @@
+import { closestCenter, DndContext, MouseSensor, TouchSensor, useSensor, useSensors } from "@dnd-kit/core";
+import { arrayMove, SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Button, Group, Input, Select, SelectProps } from "@mantine/core";
-import { useFieldArray, useForm } from "react-hook-form";
-import { FaLink, FaPlus } from "react-icons/fa";
-import { HiMiniBars2 } from "react-icons/hi2";
-import { ImLinkedin } from "react-icons/im";
-import { IoLogoYoutube } from "react-icons/io";
-import { TbBrandGithubFilled } from "react-icons/tb";
+import { Button } from "@mantine/core";
+import { useEffect, useState } from "react";
+import { FormProvider, useFieldArray, useForm } from "react-hook-form";
+import { FaPlus } from "react-icons/fa";
 import { z } from "zod";
+import { LinkFields } from "./LinkFields";
 
 const LinksValidationSchema = z.object({
 	links: z
@@ -34,41 +34,36 @@ const LinksValidationSchema = z.object({
 		.min(1, "At least one link is required"),
 });
 
-type LinksFormType = z.infer<typeof LinksValidationSchema>;
+export type LinksFormType = z.infer<typeof LinksValidationSchema>;
 
 export const Links = () => {
-	const icons: Record<string, React.ReactNode> = {
-		github: <TbBrandGithubFilled />,
-		youtube: <IoLogoYoutube />,
-		linkedin: <ImLinkedin />,
-	};
+	// const icons: Record<string, React.ReactNode> = {
+	// 	github: <TbBrandGithubFilled />,
+	// 	youtube: <IoLogoYoutube />,
+	// 	linkedin: <ImLinkedin />,
+	// };
 
-	const platformData = [
-		{ value: "github", label: "Github" },
-		{ value: "youtube", label: "Youtube" },
-		{ value: "linkedin", label: "Linkedin" },
-	];
+	// const platformData = [
+	// 	{ value: "github", label: "Github" },
+	// 	{ value: "youtube", label: "Youtube" },
+	// 	{ value: "linkedin", label: "Linkedin" },
+	// ];
 
-	const renderSelectOption: SelectProps["renderOption"] = ({ option }) => (
-		<Group flex="1" gap="xs">
-			{icons[option.value]}
-			{option.label}
-		</Group>
-	);
+	// const renderSelectOption: SelectProps["renderOption"] = ({ option }) => (
+	// 	<Group flex="1" gap="xs">
+	// 		{icons[option.value]}
+	// 		{option.label}
+	// 	</Group>
+	// );
 
-	const {
-		handleSubmit,
-		register,
-		formState: { errors },
-		reset,
-		control,
-		setValue,
-		clearErrors,
-		watch,
-	} = useForm<LinksFormType>({
+	const [items, setItems] = useState<any[]>([]);
+
+	const LinksFormMethods = useForm<LinksFormType>({
 		resolver: zodResolver(LinksValidationSchema),
 		defaultValues: { links: [{ platform: "", link: "" }] },
 	});
+
+	const { control, watch } = LinksFormMethods;
 
 	const { fields, append, remove } = useFieldArray({
 		name: "links",
@@ -84,66 +79,128 @@ export const Links = () => {
 		};
 	});
 
+	useEffect(() => {
+		setItems(
+			fields.map((field, index) => {
+				// console.log(field, watchLinksArray[index]);
+				return {
+					...field,
+					...watchLinksArray[index],
+				};
+			})
+		);
+	}, [controlledLinksFields]);
+
+	const sensors = useSensors(
+		useSensor(MouseSensor, {
+			// Require the mouse to move by 10 pixels before activating
+			activationConstraint: {
+				distance: 10,
+			},
+		}),
+		useSensor(TouchSensor, {
+			// Press delay of 250ms, with tolerance of 5px of movement
+			activationConstraint: {
+				delay: 250,
+				tolerance: 5,
+			},
+		})
+	);
+
 	const onLinksSave = (data: LinksFormType) => {
 		console.log(data);
 	};
 
+	function handleDragEnd(event) {
+		const { active, over } = event;
+
+		if (active.id !== over.id) {
+			console.log(event);
+			console.log(controlledLinksFields);
+			// setValue('links', )
+			const oldIndex = controlledLinksFields.indexOf(active.id);
+			const newIndex = controlledLinksFields.indexOf(over.id);
+
+			const newControlledLinkFields = arrayMove(items, oldIndex, newIndex);
+			setItems(newControlledLinkFields);
+
+			// setItems((items) => {
+
+			// 	return arrayMove(items, oldIndex, newIndex);
+			// });
+		}
+	}
+
 	return (
 		<>
-			<form onSubmit={handleSubmit(onLinksSave)}>
-				<div className="p-10">
-					<h1 className="font-semibold text-3xl">Customize your links</h1>
-					<p className="mt-3 text-muted">Add/edit/remove links below and then share all your profiles with the world!</p>
+			<FormProvider {...LinksFormMethods}>
+				<form onSubmit={LinksFormMethods.handleSubmit(onLinksSave)}>
+					<div className="p-10">
+						<h1 className="font-semibold text-3xl">Customize your links</h1>
+						<p className="mt-3 text-muted">Add/edit/remove links below and then share all your profiles with the world!</p>
 
-					<div className="mt-10">
-						<Button variant="outline" size="lg" className="w-full" leftSection={<FaPlus />} onClick={() => append({ platform: "", link: "" })}>
-							Add new link
-						</Button>
-					</div>
-					{controlledLinksFields?.map((item, index) => (
-						<div className="bg-background rounded-lg p-5 mt-6 space-y-3" key={item.id}>
-							<div className="flex items-center">
-								<Button variant="transparent" color="muted" className="flex items-center cursor-pointer" leftSection={<HiMiniBars2 />}>
-									Link #{index + 1}
-								</Button>
-								{index < 1 ? (
-									""
-								) : (
-									<Button variant="transparent" color="muted" className="ms-auto" classNames={{ label: "font-normal" }} onClick={() => remove(index)}>
-										Remove
-									</Button>
-								)}
-							</div>
-							<Select
-								label="Platform"
-								size="lg"
-								value={item.platform}
-								placeholder="Select your platform"
-								leftSection={icons[item.platform]}
-								data={platformData}
-								renderOption={renderSelectOption}
-								{...register(`links.${index}.platform`, { required: true })}
-								onChange={(value) => {
-									setValue(`links.${index}.platform`, value || "");
-									if (value) {
-										clearErrors(`links.${index}.platform`);
-									}
-								}}
-								error={errors?.links?.length ? errors?.links[index]?.platform?.message : ""}
-								classNames={{
-									error: "text-sm",
-								}}
-							/>
-
-							<Input.Wrapper label="Link" error={errors?.links?.length ? errors?.links[index]?.link?.message : ""}>
-								<Input type="text" size="lg" {...register(`links.${index}.link`, { required: true })} leftSection={<FaLink />} />
-							</Input.Wrapper>
+						<div className="mt-10">
+							<Button variant="outline" size="lg" className="w-full" leftSection={<FaPlus />} onClick={() => append({ platform: "", link: "" })}>
+								Add new link
+							</Button>
 						</div>
-					))}
+						<DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+							<SortableContext items={controlledLinksFields} strategy={verticalListSortingStrategy}>
+								{items?.map((item, index) => (
+									<LinkFields id={item.id} index={index} item={item} remove={remove} key={index} />
+									// <div
+									// 	className="bg-background rounded-lg p-5 mt-6 space-y-3"
+									// 	key={item.id}
+									// 	id={item.id}
+									// 	ref={setNodeRef}
+									// 	style={style}
+									// 	{...attributes}
+									// 	{...listeners}
+									// >
+									// 	<div className="flex items-center">
+									// 		<Button variant="transparent" color="muted" className="flex items-center cursor-pointer" leftSection={<HiMiniBars2 />}>
+									// 			Link #{index + 1}
+									// 		</Button>
+									// 		{index < 1 ? (
+									// 			""
+									// 		) : (
+									// 			<Button variant="transparent" color="muted" className="ms-auto" classNames={{ label: "font-normal" }} onClick={() => remove(index)}>
+									// 				Remove
+									// 			</Button>
+									// 		)}
+									// 	</div>
+									// 	<Select
+									// 		label="Platform"
+									// 		size="lg"
+									// 		value={item.platform}
+									// 		placeholder="Select your platform"
+									// 		leftSection={icons[item.platform]}
+									// 		data={platformData}
+									// 		renderOption={renderSelectOption}
+									// 		{...register(`links.${index}.platform`, { required: true })}
+									// 		onChange={(value) => {
+									// 			setValue(`links.${index}.platform`, value || "");
+									// 			if (value) {
+									// 				clearErrors(`links.${index}.platform`);
+									// 			}
+									// 		}}
+									// 		error={errors?.links?.length ? errors?.links[index]?.platform?.message : ""}
+									// 		classNames={{
+									// 			error: "text-sm",
+									// 		}}
+									// 	/>
 
-					<Input.Error>{errors?.links?.message}</Input.Error>
+									// 	<Input.Wrapper label="Link" error={errors?.links?.length ? errors?.links[index]?.link?.message : ""}>
+									// 		<Input type="text" size="lg" {...register(`links.${index}.link`, { required: true })} leftSection={<FaLink />} />
+									// 	</Input.Wrapper>
+									// </div>
+								))}
+							</SortableContext>
+						</DndContext>
 
-					{/* <div className="bg-background rounded-lg p-5 mt-6 space-y-3">
+						{/* <Input.Error>{errors?.links?.message}</Input.Error> */}
+
+						{/* <div className="bg-background rounded-lg p-5 mt-6 space-y-3">
 					<div className="flex items-center">
 						<Button variant="transparent" color="muted" className="flex items-center cursor-pointer" leftSection={<HiMiniBars2 />}>
 							Link #2
@@ -166,13 +223,14 @@ export const Links = () => {
 						<Input type="text" size="lg" leftSection={<FaLink />} />
 					</Input.Wrapper>
 				</div> */}
-				</div>
-				<div className="border-t border-gray-300 px-10 py-7 flex justify-end">
-					<Button type="submit" size="lg">
-						Save
-					</Button>
-				</div>
-			</form>
+					</div>
+					<div className="border-t border-gray-300 px-10 py-7 flex justify-end">
+						<Button type="submit" size="lg">
+							Save
+						</Button>
+					</div>
+				</form>
+			</FormProvider>
 		</>
 	);
 };
